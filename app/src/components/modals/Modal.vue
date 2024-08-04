bf2c5d44d4f54a3f4bca2680617e517ca5db8892
Dasarath G <dasarathg68@gmail.com>
2024-08-04 08:30:04 +0530

    chore: remove teleport

--- app/src/components/modals/Modal.vue
+++ app/src/components/modals/Modal.vue
@@ -1,26 +1,24 @@
 <template>
-  <Teleport to="body">
-    <dialog
-      id="my_modal_20"
-      v-if="isModalActive"
-      class="modal sm:modal-middle"
-      :class="{ 'modal-open': isModalActive }"
-    >
-      <div class="modal-box">
-        <button
-          class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-          @click="$emit('close-modal')"
-        >
-          <span>x</span>
-        </button>
-        <h1 class="font-bold text-2xl">{{ heading }}</h1>
+  <dialog
+    id="my_modal_20"
+    v-if="isModalActive"
+    class="modal sm:modal-middle"
+    :class="{ 'modal-open': isModalActive }"
+  >
+    <div class="modal-box">
+      <button
+        class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+        @click="$emit('close-modal')"
+      >
+        <span>x</span>
+      </button>
+      <h1 class="font-bold text-2xl">{{ heading }}</h1>
 
-        <div v-if="isModalActive" class="p-1">
-          <slot />
-        </div>
+      <div v-if="isModalActive" class="p-1">
+        <slot />
       </div>
-    </dialog>
-  </Teleport>
+    </div>
+  </dialog>
 </template>
 
 <script setup lang="ts">
