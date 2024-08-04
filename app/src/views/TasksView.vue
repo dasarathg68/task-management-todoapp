938c129e70b0b81ed2edb97feb9818be54278704
Dasarath G <dasarathg68@gmail.com>
2024-08-04 08:47:16 +0530

    style: adjust spacing

--- app/src/views/TasksView.vue
+++ app/src/views/TasksView.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="mt-24 ml-10">
-    <div class="text-xl">Tasks</div>
+    <div class="flex justify-between mr-2 mt-2 text-xl">
+      Tasks
+      <button class="btn btn-md btn-secondary" @click="addColumn('New column')">Add Column</button>
+    </div>
     <!-- <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20"></div>
       -->
-    <button class="btn btn-md btn-secondary" @click="addColumn('New column')">Add Column</button>
-    <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-4 gap-5">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mb-10">
       <Column v-for="column of STORE" :key="column.columnId" :column="column" />
     </div>
   </div>
